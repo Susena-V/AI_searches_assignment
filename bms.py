58f196c6dae51e21e0a7de219e2a6d671466e20a
Susena-V <145911550+Susena-V@users.noreply.github.com>
2023-09-30 18:53:38 +0530

    Update bms.py

--- bms.py
+++ bms.py
@@ -5,7 +5,7 @@ def create_Graph():
   n=int(input("Enter no of nodes: "))
   nodes=[]
 
-  G = nx.Graph()
+  G = nx.DiGraph()
 
   for i in range(n):
     node=input(f'Enter node {i+1}: ')
@@ -21,11 +21,25 @@ def create_Graph():
 
 G=create_Graph()
 
+nx.draw(G,with_labels=True,node_size=1000,node_color='purple',font_size=10,font_color='white')
+plt.show()
+
 start=input('Enter Starting node: ')
 end=input('Enter Ending node: ')
 
+all_paths=[]
+
 for path in nx.all_simple_paths(G,start,end):
+  all_paths.append(path)
   print(path)
-  
-nx.draw(G,with_labels=True,node_size=1000,node_color='purple',font_size=10,font_color='white')
-plt.show()
+
+for path in all_paths:
+    p = "--> ".join(path)
+    path_graph = G.edge_subgraph([(path[i], path[i+1]) for i in range(len(path)-1)])
+
+    pos = nx.shell_layout(path_graph)
+    nx.draw_networkx_nodes(path_graph, pos)
+    nx.draw_networkx_edges(path_graph, pos)
+    nx.draw_networkx_labels(path_graph, pos)
+    plt.title(f"Path: {'--> '.join(path)}")
+    plt.show()
